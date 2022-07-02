08d269a3840a94eff7e8ec2ebf0656b959cf5716
Mariano San Juan <marianosanjuan90@gmail.com>
2022-07-02 14:48:04 -0300

    Definicion de view_home+Creacion de carpeta Template con ruta incluida en settings.py del proyecto+Inclusion de home.html en carpeta template

--- appCTSJ/views.py
+++ appCTSJ/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+# Definición de view de la home
+
+# def view_home(request):
+#     template = loader.get_template("home.html")
+#     render = template.render({})
+#     return HttpResponse(render)
+
+# Otra alternativa para la view de la home
+
+def view_home(request):
+    return render(request,"home.html",{})
+ 
